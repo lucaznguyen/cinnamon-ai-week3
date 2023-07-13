0ee4b3576e4234caba1154cf8d3fa270d8be5c02
Lê Nhật Kha <20520208@gm.uit.edu.vn>
2023-07-13 12:57:04 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import argparse
+
+from converter import ConvertToImage
+
+"""
+The program converts a file (.png, .heic, .tiff, .pdf) to PNGs and saves them to a directory
+"""
+
+
+def convert_to_images(opt: object) -> None:
+    
+    # Conversion process
+    ConvertToImage.convert(opt.from_path, opt.to_path)
+
+
+if __name__ == "__main__":
+    """ Argument """ 
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--from_path",
+        default="example.heic",
+        help="the path of the file that needs to be converted",
+    )
+    parser.add_argument(
+        "--to_path",
+        default="./",
+        help="the path of the converted file",
+    )
+
+    opt = parser.parse_args()
+
+    convert_to_images(opt)
